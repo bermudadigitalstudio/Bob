f13a0a874bc308655be34dd24f6288f9e3545d62
Swizzlr <git@swizzlr.co>
2017-01-31 22:34:41 -0500

    Zing and pep?

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -3,4 +3,4 @@
 set -exo pipefail
 
 docker build -t swift-test -f Dockerfile~test ./
-docker run --rm swift-test || set +x; RED='\033[0;31m' NC='\033[0m' echo -e "${RED}Tests exited with non-zero exit code${NC}"; tput bel; exit 1
+docker run --rm swift-test || set +x; echo -e "\033[0;31mTests exited with non-zero exit code\033[0m"; tput bel; exit 1
